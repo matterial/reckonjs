a71a0a66cb11000efc54cec2e4d9b54b75bab83e
Riten Vagadiya <riten.s.v@gmail.com>
2014-12-01 23:03:09 +0400

    The base lib files and test

---
+++ reckon.js
@@ -0,0 +1,221 @@
+(function() {
+	'use strict';
+
+	/**
+	 * Reckon settings
+	 * @type {Object}
+	 */
+	var reckonSettings = {
+		stringPrototype: true,
+		delimStart: '{{',
+		delimEnd: '}}'
+	};
+
+	/**
+	 * Create an instance
+	 * @param {Object} params Configuration and data
+	 */
+	var Reckon = function(params) {
+		return new ReckonInstance(params);
+	}
+
+	/**
+	 * The actual Reckon implementation
+	 * @param {Object} params Configuration and data
+	 */
+	var ReckonInstance = function(params) {
+		/**
+		 * Reckon version
+		 * @type {String}
+		 */
+		this.version = '0.1.0';
+
+		/**
+		 * Define delimiter that marks beginning of interpolation
+		 * @type {String}
+		 */
+		var delimStart = reckonSettings.delimStart;
+		
+		/**
+		 * Define delimiter that marks end of interpolation
+		 * @type {String}
+		 */
+		var delimEnd = reckonSettings.delimEnd;
+
+		/**
+		 * Get the text from params
+		 * @type {String}
+		 */
+		this.text = params.text;
+
+		/**
+		 * Get the scope from the params and ensure its an array, if not, make it one
+		 * @type {Array}
+		 */
+		this.scopes = [].concat(params.scope);
+
+		/**
+		 * Compile the initial input
+		 */
+		this.compile();
+	};
+
+	/**
+	 * The reckon prototype
+	 * @type {Object}
+	 */
+	ReckonInstance.prototype = Reckon.fn = {
+
+		/**
+		 * The function responsible for interpolation
+		 * @return {Reckon} return self object for chaining
+		 */
+		compile: function() {
+			/**
+			 * The required regexp computed using delims in settings
+			 * @type {RegExp}
+			 */
+			var re = new RegExp(['/{%(.+?)%}|', this.delimStart, '(.+?)', this.delimEnd, '/g'].join(''));
+
+			/**
+			 * Reference the instance
+			 */
+			 var rInstance = this;
+
+			 /**
+			  * Save the raw text
+			  * @type {String}
+			  */
+			 rInstance.raw = rInstance.text;
+
+			/**
+			 * Compute and assign to compiledText property of the same object
+			 * @param  {String} _  Matched string
+			 * @param  {String} $1 Content of first match group
+			 * @param  {String} $2 Content of second match group
+			 * @return {String}    Interpolated string
+			 */
+			rInstance.text = rInstance.text.replace(/{%(.+?)%}|{{(.+?)}}/g, function(_, $1, $2) {
+				var computed;
+
+				/**
+				 * If escaped value is found, interpolation will not happen
+				 */
+				if ($1) {
+					computed = $1;
+				}
+
+				/**
+				 * Matched content, let's interpolate
+				 */
+				if ($2) {
+
+					/**
+					 * Break out scope variables out of scope's box and evaluate the expr expression
+					 */
+					var scopeBox = function(expr, localScope) {
+						var variables = '';
+
+						/**
+						 * If scope is a window object, no need to scopebox it
+						 */
+						if (typeof window !== "undefined" ? localScope !== window : true) {
+							for(var i in localScope) {
+								variables += 'var ' + i + ' = localScope.' + i + '; ';
+							}
+						}
+						var unbox = '(function() { ' + variables + ' try { return eval(expr);} catch(e) {} })()';
+						return eval(unbox);
+					};
+
+					if (rInstance.scopes.length) {
+						var numScopes = rInstance.scopes.length;
+						for (var i=0;i<numScopes;i++) {
+							/**
+							 * Current Scope
+							 * @type {String}
+							 */
+							var scope = rInstance.scopes[i];
+
+							/**
+							 * Get the computation
+							 * @type {Any}
+							 */
+							computed = scopeBox($2, scope);
+
+							/**
+							 * If the computed property is a function, execute it in the context of the current scope
+							 * @type {Unknown}
+							 */
+							if (typeof computed === "function") {
+								computed = computed.call(scope);
+							}
+
+							/**
+							 * If the computed property is an object, get the string
+							 */
+							if (typeof computed === "object") {
+								computed = computed.toString();
+							}
+								
+							/**
+							 * Found what we were looking for, now break the loop
+							 */
+							if (computed !== undefined)
+								break;
+						}
+					} else {
+						/**
+						 * If no scope is passed, let us assume the global scope
+						 * @type {Any}
+						 */
+						computed = scopeBox($2, window);
+					}
+
+					/**
+					 * If no computations were found, we return the raw matched value back
+					 * @type {String}
+					 */
+					computed = computed !== undefined ? computed : _;
+				}
+
+				/**
+				 * Final computed value returned
+				 */
+				return computed;
+			});
+			
+			/**
+			 * Return self for chaining
+			 */
+			return rInstance;
+		},
+		toString: function() {
+			return this.text;
+		}
+	}
+	if (typeof window !== "undefined") {
+		if (!window.reckon) {
+			window.reckon = Reckon;
+			window.reckonSettings = reckonSettings;	
+
+			/**
+			 * Add method to string's prototype for easier access
+			 */
+			if (reckonSettings.stringPrototype === true) {
+
+				/**
+				 * Reckon added to string's prototype
+				 * @param  {Array} scopes list of scope objects
+				 * @return {String}       the reckoned string
+				 */
+				String.prototype.reckon = function(scopes) {
+					return window.reckon({text: this, scope: scopes}).toString();
+				}
+			}
+		}
+	} else {
+		module.exports = Reckon;
+	}
+
+})();
---
+++ test/tests.js
@@ -0,0 +1,62 @@
+var assert = require("assert"), reckon = require("../reckon.js");
+var window = {};
+
+describe('ReckonJS', function() {
+	
+	var scope = {
+		firstName: "John",
+		lastName: "Doe",
+		age: 20,
+		ageNextYear: function() {
+			return this.age+1;
+		}
+	};
+
+	var testString = "Mr. {{firstName.substr(0, 2)}}-is a javascript enthusiast - his last name is {{lastName}}. Currently, he is {{age}} years old. Next year, he will turn {{ageNextYear}}.";
+	var reckoned = reckon({text: testString, scope: scope});
+	var withEscape = "This should not be interpolated: {%{{firstName}}%}";
+	var withEscapeReckoned = reckon({text: withEscape, scope: scope});
+
+	var withHobby = testString + " Oh, and he loves {{hobby}}.";
+
+	describe('#reckon primary functionality', function() {
+		it('should return a function', function(){
+		   assert.equal("function", typeof reckon);
+		});
+
+		it('should interpolate with variables from scope', function(){
+		   assert.notEqual(reckoned.toString().indexOf(scope.lastName), -1);
+		});
+
+		it('should execute used functions in scope', function(){
+		   assert.notEqual(reckoned.toString().indexOf(scope.ageNextYear()), -1);
+		});
+
+		it('should execute inline JS within delimiters', function(){
+		   assert.notEqual(reckoned.toString().indexOf("Mr. " + scope.firstName.substr(0, 2)), -1);
+		});
+	});
+
+	describe('#reckon escaping and more', function() {
+		it('should not have escaped variable from scope', function() {
+		   assert.notEqual(withEscapeReckoned, scope.firstName);
+		});
+
+		it('should support multiple scopes', function() {
+			var scope2 = {
+				hobby: 'Programming'
+		   	}
+		   	var multiScopeReckoned = reckon({text: withHobby, scope: [scope, scope2]});
+		   	assert.notEqual(multiScopeReckoned, scope.lastName);
+		   	assert.notEqual(multiScopeReckoned, scope2.hobby);
+		});
+	});
+
+	// describe('defines', function(){
+	// 	it('should render define', function(){
+	// 	   assert.equal("<div>http</div>", definescompiled({foo:"http"}));
+	// 	   assert.equal("<div>http:&#47;&#47;abc.com</div>", definescompiled({foo:"http://abc.com"}));
+	// 	   assert.equal("<div></div>", definescompiled({}));
+	// 	});
+	// });
+});
